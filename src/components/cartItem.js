267630d913f67e409172b0904db73f44d719a820
Hannah Brownson <hannahbrownson18@gmail.com>
2024-03-15 13:56:12 -0400

    style updates Updated image styling and added grid to replace divs. page flow is much better now

--- src/components/cartItem.js
+++ src/components/cartItem.js
@@ -1,7 +1,6 @@
 // CartItem.js
 import React, { useState } from 'react';
-import { Card, CardMedia, Typography, Button } from '@mui/material';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Card, CardActions, CardMedia, Typography, Button } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
 import axios from 'axios';
@@ -14,7 +13,7 @@ const CartItem = ({ item, updateCart, removeFromCart }) => {
       const response = await axios.put(`http://localhost:5000/update-cart-item/${item.id}`, {
         quantity: newQuantity,
       });
-  
+
       if (response.status === 200) {
         setQuantity(newQuantity);
       } else {
@@ -36,20 +35,17 @@ const CartItem = ({ item, updateCart, removeFromCart }) => {
 
   return (
     <Card style={{ maxWidth: '300px', margin: '16px' }}>
-    <CardMedia
-    component="img"
-    height="150"
-    image={item.store_item.image_path}
-    alt={item.store_item.name}
-    style={{ marginBottom: '16px' }}
-  />
+      <CardMedia
+        component="img"
+        height="50%%"
+        image={item.store_item.image_path}
+        alt={item.store_item.name}
+        style={{ marginBottom: '16px' }}
+      />
       <div style={{ padding: '16px' }}>
         <Typography variant="h6">{item.store_item.name}</Typography>
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '8px' }}>
-          <div>
-            <Typography variant="body2" color="textSecondary">
-              ${item.store_item.price}
-            </Typography>
+          <CardActions>
             <div style={{ display: 'flex', alignItems: 'center', marginTop: '8px' }}>
               <IconButton size="small" onClick={() => handleQuantityChange(quantity - 1)} disabled={quantity <= 1}>
                 -
@@ -61,7 +57,7 @@ const CartItem = ({ item, updateCart, removeFromCart }) => {
                 +
               </IconButton>
             </div>
-          </div>
+          </CardActions>
           <div>
             <Button
               variant="contained"
